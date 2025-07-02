12b0867164dd1d6ad8e10c18e295d22e7f2d331b
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-07-02 23:45:53 +0000

    feat(dashboard): home page added, Notification button, welcome message, number of pending tasks,total expenese, group members,recent activuty and assigned tasks, navigation menu

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'screens/dashboard/home-page-dart.dart';
+
+
 void main() {
   runApp(const MyApp());
 }
@@ -31,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
